4b06c9236d3327dcd8bda35c827d32b8f590d4ee
Stephen Poole <stephen.poole@redlioncanada.com>
2016-07-28 15:03:34 -0400

    Added analytics to morefeatures.feature

--- app/landing.morefeatures.feature.ts
+++ app/landing.morefeatures.feature.ts
@@ -1,40 +1,42 @@
+import {bootstrap} from 'angular2/platform/browser'
 import {Component, Input, Inject, ElementRef} from 'angular2/core'
+import {AnalyticsServiceOn} from './analytics.directive'
 
 declare var $: JQueryStatic;
 
 @Component({
     selector: 'more-features-feature',
     template: `
-    	<a *ngIf="!!link" [attr.href]="link" target="_blank"><div>
+    	<a *ngIf="!!link" href="{{link}}" target="_blank" analyticsOn="click" analyticsCategory="{{analytics.category}}" analyticsAction="{{analytics.action}}" analyticsLabel="{{analytics.label}}"><div>
 			<div>
 				<div class="hover">
-					<!-- <div class="text-container">
+					<div *ngIf="text" class="text-container">
 						<div class="text-container-cell">
 							<p class="text">{{text}}</p>
-							<p class="cta">{{cta}}</p>
+							<p class="cta" *ngIf="cta">{{cta}}</p>
 						</div>
-					</div> -->
+					</div>
 					<img src="{{image}}" alt="{{alt}}"/>
 				</div>
 			</div>
 			<p class="bottom-text" [innerHTML]="title"></p>
 		</div></a>
-
-		<div class="no-link" *ngIf="!link">
-			<div >
-				<div class="hover">
-					<!-- <div class="text-container">
+		<div *ngIf="!link">
+			<div>
+				<div class="hover" analyticsOn="mouseover" analyticsCategory="{{analytics.category}}" analyticsAction="{{analytics.action}}" analyticsLabel="{{analytics.label}}">
+					<div *ngIf="text" class="text-container">
 						<div class="text-container-cell">
 							<p class="text">{{text}}</p>
-							<p class="cta">{{cta}}</p>
+							<p class="cta" *ngIf="cta">{{cta}}</p>
 						</div>
-					</div> -->
+					</div>
 					<img src="{{image}}" alt="{{alt}}"/>
 				</div>
 			</div>
 			<p class="bottom-text" [innerHTML]="title"></p>
 		</div>
-    `
+    `,
+    directives: [AnalyticsServiceOn]
 })
 export class MoreFeaturesFeature {
     @Input() image
